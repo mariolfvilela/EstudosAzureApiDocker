8534595ca99e5d40de725622ce7d9c290e910c1b
Mário Vilela <mariolf.vilela@gmail.com>
2020-09-16 12:00:45 -0300

    Change Teste Unit

--- API/Service/CalculoFinanceiro.cs
+++ API/Service/CalculoFinanceiro.cs
@@ -6,9 +6,9 @@ namespace API.Service
         public static double CalcularValorComJurosCompostos(
             double valorEmprestimo, int numMeses, double percTaxa)
         {
-            return valorEmprestimo * Math.Pow(1 + (percTaxa / 100), numMeses); // Simulação de falha 
-            //return Math.Round(
-            //    valorEmprestimo * Math.Pow(1 + (percTaxa / 100), numMeses), 2);
+            //return valorEmprestimo * Math.Pow(1 + (percTaxa / 100), numMeses); // Simulação de falha 
+            return Math.Round(
+                valorEmprestimo * Math.Pow(1 + (percTaxa / 100), numMeses), 2);
         }
     }
 }
